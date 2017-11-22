5922522fa407bbd1c3c5742628ea6981ea5b858b
umayalveerappan <32858607+umayalveerappan@users.noreply.github.com>
2017-11-22 13:12:35 +0530

    array file created

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.Random
+public class Random
+{
+ Public static void main(String a[])
+  {
+     Random r=new Random();
+     int a[]=new int[10];
+     for(int i=0;i<10;i++)
+     {
+        a[i]=r.nextInt(50);
+     }
+     for(int n:a)
+     {
+       System.out.println(n);
+     }
+   }
+}   
